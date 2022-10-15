7890bbe1a8d9ccd8a6294493ff406193e28e74f4
Ottavio8 <paolini.ottavio8@gmail.com>
2022-10-15 13:27:46 +0200

    da migliorare palindroma

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,3 +1,15 @@
 // **Palindroma**
 // Chiedere all’utente di inserire una parola
+ const userWord = prompt("inserisci una parola");
+
 // Creare una funzione per capire se la parola inserita è palindroma.
+
+function palindroma(str) {   
+    const length = userWord.length;  
+    for (let i = 0; i < length / 2; i++) {   
+        if (userWord[i] !== userWord[length - 1 - i]) {  
+            alert("non è palindroma");  
+        }
+        alert("è palindroma")
+}} 
+const value = palindroma(userWord);
